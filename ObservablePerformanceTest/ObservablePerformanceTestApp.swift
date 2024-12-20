3f1a8ada1dcda05960b0cb2aea8dcf9901d7ef7e
kaka0913 <kabuyuu0913@gmail.com>
2024-12-20 17:29:46 +0900

    Initial Commit

---
+++ ObservablePerformanceTest/ObservablePerformanceTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservablePerformanceTestApp.swift
+//  ObservablePerformanceTest
+//
+//  Created by 株丹優一郎 on 2024/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ObservablePerformanceTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ObservablePerformanceTestTests/ObservablePerformanceTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  ObservablePerformanceTestTests.swift
+//  ObservablePerformanceTestTests
+//
+//  Created by 株丹優一郎 on 2024/12/20.
+//
+
+import Testing
+@testable import ObservablePerformanceTest
+
+struct ObservablePerformanceTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
